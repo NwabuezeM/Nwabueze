1af94c32a685ca38e6432049b9d29e03f18cc6e4
NwabuezeM <122814541+NwabuezeM@users.noreply.github.com>
2023-09-28 20:07:08 +0100

    styled the nav bar

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 const currentYear = document.getElementById('currentYear');
+const hamburger = document.querySelector('.fa-bars');
+const sidebar = document.querySelector('.nav-bar');
+const removeSidebar = document.querySelector('.fa-times');
+
+hamburger.addEventListener('click', () => {
+    sidebar.classList.add('show-sidebar');
+});
+
+removeSidebar.addEventListener('click', () => {
+    sidebar.classList.remove('show-sidebar');
+});
+
+document.addEventListener('click', (event) => {
+    if (!sidebar.contains(event.target) && hamburger !== event.target) {
+        sidebar.classList.remove('show-sidebar');
+    }
+});
+
+const mediaQuery = window.matchMedia('(min-width: 769px)');
+
+const handleMediaQueryChange = (event) => {
+    if (event.matches) {
+        sidebar.classList.remove('show-sidebar');
+    }
+};
+
+mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+handleMediaQueryChange(mediaQuery);
 currentYear.textContent = new Date().getFullYear();
